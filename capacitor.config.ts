0e09f0e80e1887ad49320c8c8d418e41d869dc3c
Ali-Gharsheen <88122412+Ali-Gharsheen@users.noreply.github.com>
2022-10-15 11:09:34 +0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'useCameraWithCap',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
